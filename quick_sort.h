ff06d76821c822b52dfcbb9f65ff1bcf095d3b25
João Soares <jsoares.public@gmail.com>
2021-03-16 19:22:54 +0000

    Update quick_sort.h

--- quick_sort.h
+++ quick_sort.h
@@ -1,8 +1,8 @@
 #ifndef __H_QUICK_SORT__
 #define __H_QUICK_SORT__
 
-void swap(double **points, int n_dims, int a, int b);
-int partition(double **arr, int low, int high, int dim, int n_dims);
-void quick_sort(double **arr, int low, int high, int dim, int n_dims);
+void swap(double **points, int n_dims, long a, long b, long* set);
+long partition(double **arr, long low, long high, int dim, int n_dims, long* set);
+void quick_sort(double **arr, long low, long high, int dim, int n_dims, long* set);
 
 #endif
