4058ecb5cdd2e990da1d098c9481338c5ab17fb6
João Soares <jsoares.public@gmail.com>
2021-03-16 19:23:25 +0000

    Update gen_points.h

--- gen_points.h
+++ gen_points.h
@@ -1,6 +1,7 @@
 #define RANGE 10
 
 extern void print_point(double *, int);
+extern void print_point_indent(double *, int, int);
 double **create_array_pts(int n_dims, long np);
 double **get_points(int argc, char *argv[], int *n_dims, long *np);
 void print_points(double** points, int np, long n_dims);
